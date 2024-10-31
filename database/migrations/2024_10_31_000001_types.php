260c8c2431101c0f044ac9e185b1df0deee65aab
FernandoROL <felhopez@gmail.com>
2024-10-31 15:01:52 -0300

    migration date fix

--- database/migrations/2024_10_31_000001_types.php
+++ database/migrations/2024_10_31_000001_types.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create("types", function (Blueprint $table) {
             $table->id();
             $table->string('nome')->unique();
-            $table->foreignId('id_usuario')->constrained('usuarios');
+            $table->foreignId('userID')->constrained('users');
             $table->timestamps();
         });
     }
